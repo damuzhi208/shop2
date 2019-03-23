04d456c3293b4d149c9e8c3bc4752f2669bc5fec
luqi <luqi>
2019-03-23 21:00:56 +0800

    工具方法

--- src/com/company/hxs/base/dao/SqlCommonDao.java
+++ src/com/company/hxs/base/dao/SqlCommonDao.java
@@ -1,12 +1,19 @@
 package com.company.hxs.base.dao;
 
+import org.springframework.orm.hibernate4.HibernateCallback;
 import org.springframework.orm.hibernate4.HibernateOperations;
 import org.springframework.orm.hibernate4.HibernateTemplate;
 
 import java.io.Serializable;
+import java.util.List;
 
 import javax.annotation.Resource;
+
+import org.hibernate.HibernateException;
+import org.hibernate.Query;
+import org.hibernate.Session;
 import org.hibernate.SessionFactory;
+import org.hibernate.transform.Transformers;
 import org.springframework.stereotype.Repository;
 
 @Repository
@@ -40,4 +47,32 @@ public class SqlCommonDao extends HibernateTemplate implements ISqlCommonDao, Hi
 		this.hibernateTemplate.delete(entity);
 	}
 
+	public <TB> List<TB> findListBySqlAsAliasToBean2(String sql, Class<TB> beanType){
+		return findListBySqlAsAliasToBean2(sql, beanType, null, null, null);
+	}
+	
+	public <TB> List<TB> findListBySqlAsAliasToBean2(String sql, Class<TB> beanType, Object[] params){
+	    return findListBySqlAsAliasToBean2(sql, beanType, params, null, null);
+	}
+	
+	@SuppressWarnings({ "unchecked", "rawtypes" })
+	public <TB> List<TB> findListBySqlAsAliasToBean2(final String sql, final Class<TB> beanType, final Object[] params, final Integer page, final Integer size) {
+		return (List<TB>) this.hibernateTemplate.execute(new HibernateCallback() {
+			@Override  
+            public Object doInHibernate(Session session) throws HibernateException {
+				Query query = session.createSQLQuery(sql);
+				if(params != null){
+					for (int i = 0; i < params.length; i++) {
+						query.setParameter(i, params[i]);
+					}
+				}
+				if(page != null && size != null){
+					query.setFirstResult((page - 1)*size);
+					query.setMaxResults(size);
+				}
+				query.setResultTransformer(Transformers.aliasToBean(beanType));
+                return (List<TB>)query.list();  
+            }  
+       });
+	  }
 }
